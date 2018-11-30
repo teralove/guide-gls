8d6722014ed36e557f308356b19f3eab6bbc26cf
teralove <terascripts@gmail.com>
2018-11-30 14:12:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
 String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` };
-const Vec3 = require('tera-vec3');
 
-const mapID = [9782, 9982];
-const HuntingZn = [782, 982];
-const BossID = [1000, 2000, 3000];
+const Vec3 = require('tera-vec3'),
+	mapID = [9782, 9982],
+	HuntingZn = [782, 982],
+	BossID = [1000, 2000, 3000],
+	config = require('./config.json'),
+	FirstBossActions = {
+		106: { msg: 'Heavy' },
+		107: { msg: 'Pushback' },
+		109: { msg: 'Rocks (Small)' },
+		110: { msg: 'Rocks (Large)' },
+		301: { msg: 'Flower stuns' },
+		309: { msg: '1 flower' },
+		310: { msg: '2 flowers' },
+		116: { msg: 'Big AoE attack!!' },
+		312: { msg: 'Golden flower!!' }
+	}, SecondBossActions = {
+		105: { msg: 'Spin' },
+		113: { msg: 'Stun inc' },
+		114: { msg: 'Get IN' },
+		116: { msg: 'Front then Back' },
+		301: { msg: '↓ Get OUT + dodge' },
+		302: { msg: '↑ Get IN + dodge' }
+	}, ThirdBossActions = {
+		118: { msg: 'Front triple' }, // aka Tank Buster
+		143: { msg: '←← Left rear ←←' },
+		145: { msg: '←← Left rear ←←' },
+		146: { msg: '←← Left rear ←← (pulses)', sign_degrees: 330, sign_distance: 320 },
+		154: { msg: '←← Left rear ←← (pulses)', sign_degrees: 330, sign_distance: 320 },
+		144: { msg: '→→ Right rear →→' },
+		147: { msg: '→→ Right rear →→' },
+		148: { msg: '→→ Right rear →→ (pulses)', sign_degrees: 30, sign_distance: 320 },
+		155: { msg: '→→ Right rear →→ (pulses)', sign_degrees: 30, sign_distance: 320 },
 
-const config = require('./config.json');
-const FirstBossActions = {
-	106: {msg: 'Heavy'},
-	107: {msg: 'Pushback'},
-	109: {msg: 'Rocks (Small)'},
-	110: {msg: 'Rocks (Large)'},
-	301: {msg: 'Flower stuns'},
-	309: {msg: '1 flower'},
-	310: {msg: '2 flowers'},
-	116: {msg: 'Big AoE attack!!'},
-	312: {msg: 'Golden flower!!'}
-};
-const SecondBossActions = {
-	105: {msg: 'Spin'},
-	113: {msg: 'Stun inc'},
-	114: {msg: 'Get IN'},
-	116: {msg: 'Front then Back'},
-	301: {msg: '↓ Get OUT + dodge'},
-	302: {msg: '↑ Get IN + dodge'}
-};
-const ThirdBossActions = {
-	118: {msg: 'Front triple'}, // aka Tank Buster
-	143: {msg: '←← Left rear ←←'},
-	145: {msg: '←← Left rear ←←'},
-	146: {msg: '←← Left rear ←← (pulses)', sign_degrees: 330, sign_distance: 320},
-	154: {msg: '←← Left rear ←← (pulses)', sign_degrees: 330, sign_distance: 320},
-	144: {msg: '→→ Right rear →→'},
-	147: {msg: '→→ Right rear →→'},
-	148: {msg: '→→ Right rear →→ (pulses)', sign_degrees: 30, sign_distance: 320},
-	155: {msg: '→→ Right rear →→ (pulses)', sign_degrees: 30, sign_distance: 320},
-	
-	139: {msg: 'Left safe', sign_degrees: 270, sign_distance: 200}, //151 //clockwise (swinging head) king hitting right
-	150: {msg: 'Left safe!', sign_degrees: 270, sign_distance: 200}, //151 //clockwise (landing) king hit right
-	141: {msg: 'Right safe', sign_degrees: 90, sign_distance: 200}, //153 //Counterclockwise (swinging head) King hit left
-	152: {msg: 'Right safe!', sign_degrees: 90, sign_distance: 200}, //153 //Counterclockwise (landing) King hit left
-	
-	161: {msg: 'Back then Front'},
-	162: {msg: 'Back then Front'},
-	213: {msg: 'Tail'},
-	215: {msg: 'Tail!!'},
-	
-	300: {msg: 'Dodge!! (Awakening 1)'},
-	360: {msg: 'Explosion!!'},
-	399: {msg: 'Dodge!! (Awakening 2)'}
-};
+		139: { msg: 'Left safe', sign_degrees: 270, sign_distance: 200 }, //151 //clockwise (swinging head) king hitting right
+		150: { msg: 'Left safe!', sign_degrees: 270, sign_distance: 200 }, //151 //clockwise (landing) king hit right
+		141: { msg: 'Right safe', sign_degrees: 90, sign_distance: 200 }, //153 //Counterclockwise (swinging head) King hit left
+		152: { msg: 'Right safe!', sign_degrees: 90, sign_distance: 200 }, //153 //Counterclockwise (landing) King hit left
 
-module.exports = function GrottoOfLostSoulsGuide(d) {
-	let	enabled = config.enabled,
+		161: { msg: 'Back then Front' },
+		162: { msg: 'Back then Front' },
+		213: { msg: 'Tail' },
+		215: { msg: 'Tail!!' },
+
+		300: { msg: 'Dodge!! (Awakening 1)' },
+		360: { msg: 'Explosion!!' },
+		399: { msg: 'Dodge!! (Awakening 2)' }
+	};
+
+module.exports = function GrottoOfLostSoulsGuide(mod) {
+	let enabled = config.enabled,
 		sendToParty = config.sendToParty,
 		streamenabled = config.streamenabled,
 		msgcolour = config.msgcolour,
-
 		isTank = false,
 		insidemap = false,
 		insidezone = false,
@@ -64,82 +60,99 @@ module.exports = function GrottoOfLostSoulsGuide(d) {
 		whichboss = 0,
 		hooks = [], bossCurLocation, bossCurAngle, uid0 = 999999999, uid1 = 899999999, uid2 = 799999999, notice = true, power = false, Level = 0, powerMsg = '';
 
-	d.command.add('ddinfo', (arg) => {
-		d.command.message('enabled: ' + `${enabled}`.clr('00FFFF'));
-		d.command.message('insidemap: ' + insidemap);
-		d.command.message('insidezone: ' + insidezone);
-		d.command.message('whichmode: ' + whichmode);
-		d.command.message('whichboss: ' + whichboss);
-		d.command.message('sendToParty ' + (sendToParty ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
-		d.command.message('isTank ' + (isTank ? 'true'.clr('00FFFF') : 'false'.clr('FF0000')));
+	mod.command.add('ddinfo', () => {
+		mod.command.message(`enabled: ${enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')}.
+		insidemap: ${insidemap}.
+		insidezone: ${insidezone}.
+		whichmode: ${whichmode}.
+		whichboss: ${whichboss}.
+		sendToParty: ${sendToParty ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')}.
+		isTank: ${isTank ? 'true'.clr('00FFFF') : 'false'.clr('FF0000')}.`);
 		sendMessage('test');
 	})
-	d.command.add('gls', (arg) => {
+
+	mod.command.add('gls', (arg) => {
 		if (!arg) {
 			enabled = !enabled;
-			d.command.message('enabled: ' + (enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
-		} else {
-			switch (arg) {
-				case "party":
-					sendToParty = !sendToParty;
-					d.command.message('sendToParty ' + (sendToParty ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
-					break;
-				case "proxy":
-					streamenabled = !streamenabled;
-					d.command.message('streamenabled ' + (streamenabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
-					break;
-				default :
-					d.command.message('Invalid argument!'.clr('FF0000'));
-					break;
-			}
+			mod.command.message('enabled: ' + (enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+			return;
+		}
+		arg = arg.toLowerCase();
+		switch (arg) {
+            case "on":
+                enabled = true;
+                mod.command.message('enabled: ' + (enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+                break;
+            case "off":
+                enabled = false;
+                mod.command.message('enabled: ' + (enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+                break;
+			case "party":
+				sendToParty = !sendToParty;
+				mod.command.message('sendToParty ' + (sendToParty ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+				break;
+			case "proxy":
+			case "self":
+            case "stream":
+				streamenabled = !streamenabled;
+				mod.command.message('streamenabled ' + (streamenabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+				break;
+			case "info":
+				mod.command.message(`enabled: ${enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')}.
+		insidemap: ${insidemap}.
+		insidezone: ${insidezone}.
+		whichmode: ${whichmode}.
+		whichboss: ${whichboss}.
+		sendToParty: ${sendToParty ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')}.
+		isTank: ${isTank ? 'true'.clr('00FFFF') : 'false'.clr('FF0000')}.`);
+				sendMessage('test');
+				break;
+			default:
+				enabled = !enabled;
+				mod.command.message('enabled: ' + (enabled ? 'true'.clr('56B4E9') : 'false'.clr('E69F00')));
+				break;
 		}
 	});
 
-	d.hook('S_LOGIN', 10, sLogin)
-	d.hook('S_LOAD_TOPO', 3, sLoadTopo);
+	mod.hook('S_LOGIN', 10, sLogin)
+	mod.hook('S_LOAD_TOPO', 3, sLoadTopo);
 
 	function sLogin(event) {
 		let job = (event.templateId - 10101) % 100;
 		if (job === 1 || job === 10) {
 			isTank = true;
-		} else {
-			isTank = false;
-		}
+		} else isTank = false;
 	}
 
 	function sLoadTopo(event) {
-		if (event.zone === mapID[0]) {								
+		if (event.zone === mapID[0]) {
 			insidemap = true;
-			d.command.message('Welcome to ' + 'Grotto of Lost Souls '.clr('56B4E9') + '[NM]'.clr('E69F00'));
+			mod.command.message('Welcome to ' + 'Grotto of Lost Souls '.clr('56B4E9') + '[NM]'.clr('E69F00'));
 			load();
 		} else if (event.zone === mapID[1]) {
 			insidemap = true;
-			d.command.message('Welcome to ' + 'Grotto of Lost Souls '.clr('56B4E9') + '[HM]'.clr('00FFFF'));
+			mod.command.message('Welcome to ' + 'Grotto of Lost Souls '.clr('56B4E9') + '[HM]'.clr('00FFFF'));
 			load();
-		} else {
-			unload();
-		}
-    }
+		} else unload();
+	}
 
 	function load() {
 		if (!hooks.length) {
 			hook('S_BOSS_GAGE_INFO', 3, sBossGageInfo);
 			hook('S_ACTION_STAGE', 8, sActionStage);
-			hook('S_DUNGEON_EVENT_MESSAGE', 2,sDungeonEventMessage);
+			hook('S_DUNGEON_EVENT_MESSAGE', 2, sDungeonEventMessage);
 
 			function sBossGageInfo(event) {
 				if (!insidemap) return;
 
-				let bosshp = (event.curHp / event.maxHp);
+				let bosshp = (Number(event.curHp) / Number(event.maxHp));
 
-				if (bosshp <= 0) {
-					whichboss = 0;
-				}
+				if (bosshp <= 0) whichboss = 0;
 
-				if (event.curHp == event.maxHp) {
-					notice = true,
-					power = false,
-					Level = 0,
+				if (Number(event.curHp) == Number(event.maxHp)) {
+					notice = true;
+					power = false;
+					Level = 0;
 					powerMsg = '';
 				}
 
@@ -161,22 +174,21 @@ module.exports = function GrottoOfLostSoulsGuide(d) {
 			}
 
 			function sActionStage(event) {
-				if (!enabled || !insidezone || whichboss==0) return;
-				if (event.templateId!=BossID[0] && event.templateId!=BossID[1] && event.templateId!=BossID[2]) return;
+				if (!enabled || !insidezone || whichboss == 0) return;
+				if (event.templateId != BossID[0] && event.templateId != BossID[1] && event.templateId != BossID[2]) return;
 				let skillid = event.skill.id % 1000;
 				bossCurLocation = event.loc;
 				bossCurAngle = event.w;
 
-				if (whichboss==1 && FirstBossActions[skillid]) {
+				if (whichboss == 1 && FirstBossActions[skillid]) {
 					if (!isTank && skillid === 106) return;
-					if ( isTank && skillid === 107) return;
+					if (isTank && skillid === 107) return;
 					sendMessage(FirstBossActions[skillid].msg);
 				}
 
-				if (whichboss==2 && SecondBossActions[skillid]) {
+				if (whichboss == 2 && SecondBossActions[skillid]) {
 					sendMessage(SecondBossActions[skillid].msg);
-					// 2王 内外圈
-					if (skillid === 114 || skillid === 301 || skillid === 302) {
+					if ([114, 301, 302].includes(skillid)) {
 						Spawnitem(603, 20, 260);
 						Spawnitem(603, 40, 260);
 						Spawnitem(603, 60, 260);
@@ -196,7 +208,6 @@ module.exports = function GrottoOfLostSoulsGuide(d) {
 						Spawnitem(603, 340, 260);
 						Spawnitem(603, 360, 260);
 					}
-					// 2王 前砸后砸 横向对称轴
 					if (skillid === 116) {
 						Spawnitem(603, 90, 25);
 						Spawnitem(603, 90, 50);
@@ -242,56 +253,25 @@ module.exports = function GrottoOfLostSoulsGuide(d) {
 					}
 				}
 
-				if (whichboss==3 && ThirdBossActions[skillid]) {
-					// 屏蔽重复通知的技能
+				if (whichboss == 3 && ThirdBossActions[skillid]) {
 					if (!notice) return;
-					if (notice && (skillid===118||skillid===139||skillid===150||skillid===141||skillid===152)) {
+					if (notice && [118, 139, 141, 150, 152].includes(skillid)) {
 						notice = false;
-						setTimeout(function() { notice = true }, 4000);
+						setTimeout(() => notice = true, 4000);
 					}
-					// 蓄电层数计数
-					if (whichmode==2) {
-						// 一次觉醒 开始充能计数
-						if (skillid===300) power = true, Level = 0, powerMsg = '';
-						// 放电爆炸 重置充能计数
-						if (skillid===360) Level = 0;
-						// 二次觉醒 开始充能计数
-						if (skillid===399) Level = 0;
+					if (whichmode == 2) {
+						if (skillid === 300) power = true, Level = 0, powerMsg = '';
+						if (skillid === 360 || skillid === 399) Level = 0;
 					}
-					if (power && (
-						skillid===118||
-
-						skillid===213||
-						skillid===215||
-
-						skillid===143||
-						skillid===145||
-
-						skillid===146||
-						skillid===154||
-
-						skillid===144||
-						skillid===147||
-
-						skillid===148||
-						skillid===155||
-
-						skillid===161||
-						skillid===162)
-					) {
+					if (power && [118, 143, 145, 146, 144, 147, 148, 154, 155, 161, 162, 213, 215].includes(skillid)) {
 						Level++;
 						//powerMsg = '<font color="#FF0000">(' + Level + ') </font> ';
-						powerMsg = '{' + Level + '} ';
+						powerMsg = `${Level}`;
 					}
 
-					// 3王 左右扩散初始位置标记
-					if (skillid === 146 || skillid === 154 || skillid === 148 || skillid === 155) {
-						SpawnThing(ThirdBossActions[skillid].sign_degrees, ThirdBossActions[skillid].sign_distance, 8000);
-					}
+					if ([146, 148, 154, 155].includes(skillid)) SpawnThing(ThirdBossActions[skillid].sign_degrees, ThirdBossActions[skillid].sign_distance, 8000);
 
-					// 3王 飞天半屏攻击
-					if (skillid === 139 || skillid === 150 || skillid === 141 || skillid === 152) {
-						// 垂直对称轴
+					if ([139, 141, 150, 152].includes(skillid)) {
 						Spawnitem(603, 0, 25);
 						Spawnitem(603, 0, 50);
 						Spawnitem(603, 0, 75);
@@ -333,146 +313,128 @@ module.exports = function GrottoOfLostSoulsGuide(d) {
 						Spawnitem(603, 180, 450);
 						Spawnitem(603, 180, 475);
 						Spawnitem(603, 180, 500);
-						// 光柱+告示牌
 						SpawnThing(ThirdBossActions[skillid].sign_degrees, ThirdBossActions[skillid].sign_distance, 5000);
 					}
-
 					sendMessage(powerMsg + ThirdBossActions[skillid].msg);
 				}
 			}
 
 			function sDungeonEventMessage(event) {
-				if (!enabled || !insidezone || whichboss==0) return;
-				let sDungeonEventMessage = parseInt(event.message.replace('@dungeon:', ''));
-				if (whichboss==3) {
-					if (sDungeonEventMessage === 0000000) {
-						sendMessage('!!');
-					}
-				}
+				if (!enabled || !insidezone || whichboss == 0) return;
+				let sDungeonEventMessage = event.message.replace('@dungeon:', '');
+				if (whichboss == 3 && sDungeonEventMessage == '0000000') sendMessage('!!');
 			}
 		}
 	}
 
 	function hook() {
-		hooks.push(d.hook(...arguments));
+		hooks.push(mod.hook(...arguments));
 	}
 
 	function unload() {
 		if (hooks.length) {
-			for (let h of hooks)
-				d.unhook(h);
+			for (let h of hooks) mod.unhook(h);
 			hooks = [];
 		}
 		reset();
 	}
 
 	function reset() {
-		insidemap = false,
-		insidezone = false,
-		whichmode = 0,
-		whichboss = 0,
-		notice = true,
-		power = false,
-		Level = 0,
+		insidemap = false;
+		insidezone = false;
+		whichmode = 0;
+		whichboss = 0;
+		notice = true;
+		power = false;
+		Level = 0;
 		powerMsg = '';
 	}
 
 	function sendMessage(msg) {
-		if (msgcolour) {
-			msg = `${msg}`.clr(msgcolour);
-		}
+		if (msgcolour) msg = `${msg}`.clr(msgcolour);
 
 		if (sendToParty) {
-			d.toServer('C_CHAT', 1, {
+			mod.send('C_CHAT', 1, {
 				channel: 21, //21 = p-notice, 1 = party, 2 = guild
 				message: msg
 			});
 		} else if (streamenabled) {
-			d.command.message(msg);
+			mod.command.message(msg);
 		} else {
-			d.toClient('S_CHAT', 2, {
+			mod.send('S_CHAT', 2, {
 				channel: 21, //21 = p-notice, 1 = party
 				authorName: 'DG-Guide',
 				message: msg
 			});
 		}
 	}
-	//地面提示(花朵)
-	function Spawnitem(item, degrees, radius) { //显示物品 偏移角度 半径距离
+
+	function Spawnitem(item, degrees, radius) {
 		if (streamenabled) return;
 		let r = null, rads = null, finalrad = null, spawnx = null, spawny = null, pos = null;
 
 		r = bossCurAngle - Math.PI;
-		rads = (degrees * Math.PI/180);
+		rads = (degrees * Math.PI / 180);
 		finalrad = r - rads;
 		spawnx = bossCurLocation.x + radius * Math.cos(finalrad);
 		spawny = bossCurLocation.y + radius * Math.sin(finalrad);
-		pos = {x:spawnx, y:spawny};
-		// 花朵
-		d.toClient('S_SPAWN_COLLECTION', 4, {
-			gameId : uid0,
-			id : item,
-			amount : 1,
-			loc : new Vec3(pos.x, pos.y, bossCurLocation.z),
-			w : r,
-			unk1 : 0,
-			unk2 : 0
+		pos = { x: spawnx, y: spawny };
+		mod.send('S_SPAWN_COLLECTION', 4, {
+			gameId: uid0,
+			id: item,
+			amount: 1,
+			loc: new Vec3(pos.x, pos.y, bossCurLocation.z),
+			w: r,
+			unk1: 0,
+			unk2: 0
 		});
-		// 延时消除
 		setTimeout(Despawn, 5000, uid0)
 		uid0--;
 	}
-	// 消除花朵
 	function Despawn(uid_arg0) {
-		d.toClient('S_DESPAWN_COLLECTION', 2, {
-			gameId : uid_arg0
+		mod.send('S_DESPAWN_COLLECTION', 2, {
+			gameId: uid_arg0
 		});
 	}
-	// 地面提示(光柱+告示牌)
-	function SpawnThing(degrees, radius, times) { //偏移角度 半径距离 持续时间
+	function SpawnThing(degrees, radius, times) {
 		if (streamenabled) return;
-		let r = null, rads = null, finalrad = null, pos = null;
+		let r = null, rads = null, finalrad = null;
 
 		r = bossCurAngle - Math.PI;
-		rads = (degrees * Math.PI/180);
+		rads = (degrees * Math.PI / 180);
 		finalrad = r - rads;
 		bossCurLocation.x = bossCurLocation.x + radius * Math.cos(finalrad);
 		bossCurLocation.y = bossCurLocation.y + radius * Math.sin(finalrad);
-		// 告示牌
-		d.toClient('S_SPAWN_BUILD_OBJECT', 2, {
-			gameId : uid1,
-			itemId : 1,
-			loc : bossCurLocation,
-			w : r,
-			unk : 0,
-			ownerName : 'prompt',
-			message : 'Prompt area'
+		mod.send('S_SPAWN_BUILD_OBJECT', 2, {
+			gameId: uid1,
+			itemId: 1,
+			loc: bossCurLocation,
+			w: r,
+			unk: 0,
+			ownerName: 'prompt',
+			message: 'Prompt area'
 		});
 
 		bossCurLocation.z = bossCurLocation.z - 100;
-		// 龙头光柱
-		d.toClient('S_SPAWN_DROPITEM', 6, {
+		mod.send('S_SPAWN_DROPITEM', 6, {
 			gameId: uid2,
 			loc: bossCurLocation,
 			item: 98260,
 			amount: 1,
 			expiry: 6000,
-			owners: [{playerId: uid2}]
+			owners: [{ playerId: uid2 }]
 		});
-		// 延迟消除
 		setTimeout(DespawnThing, times, uid1, uid2);
 		uid1--;
 		uid2--;
 	}
-	// 消除 光柱+告示牌
 	function DespawnThing(uid_arg1, uid_arg2) {
-		d.toClient('S_DESPAWN_BUILD_OBJECT', 2, {
-			gameId : uid_arg1,
-			unk : 0
+		mod.send('S_DESPAWN_BUILD_OBJECT', 2, {
+			gameId: uid_arg1,
+			unk: 0
 		});
-		d.toClient('S_DESPAWN_DROPITEM', 4, {
+		mod.send('S_DESPAWN_DROPITEM', 4, {
 			gameId: uid_arg2
 		});
 	}
-
 }
